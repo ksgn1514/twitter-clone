90a410a191659377b98516b97c9c5c5ae4e80591
kim.sg <ksgn1514@gmail.com>
2023-10-17 22:59:40 +0900

    firebase setup

--- src/fireabase.ts
+++ src/fireabase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsWrY5YzTJe3e-x6TcrPc_jdsDOLJOR4U",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
