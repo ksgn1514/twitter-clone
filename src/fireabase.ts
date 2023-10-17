7e66f6449f76f551a23ac59d8f80759eb03ae90c
kim.sg <ksgn1514@gmail.com>
2023-10-17 22:54:45 +0900

    routing, firebase init

---
+++ src/fireabase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsWrY5YzTJe3e-x6TcrPc_jdsDOLJOR4U",
+  authDomain: "twitter-clone-b99d6.firebaseapp.com",
+  projectId: "twitter-clone-b99d6",
+  storageBucket: "twitter-clone-b99d6.appspot.com",
+  messagingSenderId: "783604362717",
+  appId: "1:783604362717:web:87d5ca2554851faee3da34",
+};
+
+const app = initializeApp(firebaseConfig);
