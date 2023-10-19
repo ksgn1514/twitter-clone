c35b4c98c01d1e21faedb30c942ead61d6a21f3c
kim.sg <ksgn1514@gmail.com>
2023-10-19 22:10:31 +0900

    post tweet form, tweeting to firestore

--- src/fireabase.ts
+++ src/fireabase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDsWrY5YzTJe3e-x6TcrPc_jdsDOLJOR4U",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage();
+
+export const db = getFirestore();
